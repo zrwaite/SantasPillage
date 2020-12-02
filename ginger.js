c31912bf2316b2bc10bbe3239fd96ee1e942dbe1
weiqixu05 <59126263+weiqixu05@users.noreply.github.com>
2020-12-01 22:11:07 -0500

    Create ginger.js

---
+++ ginger.js
@@ -0,0 +1,75 @@
+import Sprite from "./sprite.js"
+export default class Ginger extends Sprite{
+  constructor(...args){
+    super(...args)
+    this.images = {"s":[document.getElementById("img-ginger"),document.getElementById("img-ginger")],
+    "j":[document.getElementById("img-ginger"),document.getElementById("img-ginger")],
+    "wl":[document.getElementById("img-ginger"),document.getElementById("img-ginger")],
+    "wr":[document.getElementById("img-ginger"),document.getElementById("img-ginger")]
+    }
+    this.dirs = {
+      left: 0,
+      right: 1
+    }
+    this.mstate = "s";
+    let num = Math.floor(Math.random() * 2)
+    if (num === 0){this.dir = this.dirs.right}
+    else {this.dir = this.dirs.left}
+    this.image = this.images[this.mstate][this.dir]
+    this.height = 60
+    this.width = 40
+    this.legSpeed = 12
+    this.canJump = false
+    this.speed = {
+      x:0,
+      y:0,
+      maxx:5,
+      maxy:13
+    }
+    this.reset()
+  }
+  reset(){
+    this.pos.x = this.startPos.x
+    this.pos.y = this.startPos.y
+    this.speed.y = 0
+    this.speed.x = 0
+  }
+  update(deltaTime) {
+    if (this.pos.y + this.height >= this.gHeight) {this.canJump = true}
+    this.speedControl()
+    this.xDetect()
+    super.update()
+  }
+  speedControl(){
+    if (this.dir === this.dirs.right){
+      if (this.canJump){
+        this.speed.x = this.speed.maxx
+      } else {
+        this.speed.x = 1
+      }
+    } else {
+      if (this.canJump){
+        this.speed.x = -this.speed.maxx
+      } else {
+        this.speed.x = -1
+      }
+    }
+
+  }
+  xDetect(){
+    if (this.realPos + this.width >= this.game.levelLen+800){
+      this.dir = this.dirs.left
+    }
+    if (this.realPos <=0){
+      this.realPos = 0
+      this.dir = this.dirs.right
+    }
+  }
+  jump() {
+    if (this.canJump) {
+      this.speed.y = -this.speed.maxy
+      this.canJump = false
+    }
+  }
+
+}
